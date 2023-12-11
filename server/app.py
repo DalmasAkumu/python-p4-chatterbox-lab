588377bd5ff48dc9a5e6a4f9a97d02c15efb0c5d
Dalmas <akumuodhiambo@gmail.com>
2023-12-11 09:48:15 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, jsonify
+
+from flask import Flask, request, make_response, jsonify, json
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -14,13 +15,72 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.order_by(Message.created_at.asc()).all()]
+        
+
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+        return response
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body = data["body"],
+            username = data["username"]
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_message.to_dict()),
+            201
+        )
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    update = Message.query.filter_by(id=id).first()
+
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(update,attr,data[attr])
+
+            db.session.add(update)
+            db.session.commit()
+
+            response = make_response(
+                jsonify(update.to_dict()),
+                200
+            )
+            return response
+  
+    elif request.method == 'DELETE':
+        
+        db.session.delete(update)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(
+               
+                {
+                "delete_successful" : True,
+                "message": "Message deleted."
+            }
+            ),
+            200
+        )
+       
+        return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
